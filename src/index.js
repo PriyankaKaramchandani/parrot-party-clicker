853f61d129ea0d59d7a239467cd1fa773b2438b2
Priyanka <pri.ppk@gmail.com>
2024-11-22 12:08:02 -0800

    Add parot-party-clicker event-handler

--- src/index.js
+++ src/index.js
@@ -13,3 +13,38 @@
 // Box 2: When the spacebar is pressed down, make the text in Box 2 turn into a flan emoji
 // Box 3: When Box 3 is clicked on, make Party Parrot appear and disappear
 // Box 4: When Box 4 is double clicked, an alert shows up
+
+const state = {
+  addParrotImg: null,
+  parrotCountLabel: null,
+  parrotCount: 0,
+  addResetButton: null,
+};
+
+const loadControls = () => {
+  state.addParrotImg = document.querySelector('img');
+  state.parrotCountLabel = document.getElementById('parrot-count-label');
+  state.addResetButton = document.getElementById('reset-button');
+};
+
+const registerEventHandlers = () => {
+  loadControls();
+  state.addParrotImg.addEventListener('click', () => {
+    console.log('click');
+    ++state.parrotCount;
+    state.parrotCountLabel.textContent = state.parrotCount;
+    if (state.parrotCount === 5) {
+        state.addParrotImg.width = 400;
+        state.addParrotImg.height = 400;
+    }
+  });
+
+  state.addResetButton.addEventListener('click', () => {
+    state.parrotCount = 0;
+    state.parrotCountLabel.textContent = state.parrotCount;
+    state.addParrotImg.width = 128;
+    state.addParrotImg.height = 128;
+  })
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
